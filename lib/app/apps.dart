41eedfdb1253ded0afbe381c8f20a30aa9df0102
User <shounbabu31@gmail.com>
2026-03-08 04:11:33 +0600

    Initial commit

---
+++ lib/app/apps.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyApps extends StatelessWidget {
+  const MyApps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:task_manager_apps/app/apps.dart';
+
+void main(){
+  runApp(MyApps());
+}
