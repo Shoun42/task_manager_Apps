0d0c5b76a2d8d0a03337c284c223592fc51968e2
User <shounbabu31@gmail.com>
2026-03-14 23:26:51 +0600

    update

--- lib/app/app_pages.dart
+++ lib/app/app_pages.dart
@@ -1,6 +1,11 @@
 class AppPages {
 
-  static final splashScreen='/splash';
-  static final loginScreen='/loginScreen';
+  static final String splashScreen='/splash';
+  static final String loginScreen='/loginScreen';
+  static final String emailScreen='/EmailScreen';
+  static final String pinVerification='/PinVerificationScreen';
+  static final String setPasswordScreen='/SetPasswordScreen';
+  static final String signUpView='/SingUPView';
+  static final String newTaskView='/NewTaskView';
 
 }
--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -1,8 +1,15 @@
 import 'package:get/get.dart';
 import 'package:task_manager_apps/app/app_pages.dart';
 import 'package:task_manager_apps/feature/screen/bindings/login_screen_binding.dart';
+import 'package:task_manager_apps/feature/screen/bindings/profile_task_binding.dart';
 import 'package:task_manager_apps/feature/screen/bindings/splash_binding.dart';
+import 'package:task_manager_apps/feature/screen/controller/new_task_controller.dart';
+import 'package:task_manager_apps/feature/screen/view/Email_Screen/email_screen.dart';
+import 'package:task_manager_apps/feature/screen/view/PinVarificationScreen/pin_verification_screen.dart' show PinVerificationScreen;
+import 'package:task_manager_apps/feature/screen/view/Set_Password/set_password.dart';
 import 'package:task_manager_apps/feature/screen/view/login_screen/login_screen.dart';
+import 'package:task_manager_apps/feature/screen/view/new%20task%20screen/new_task_screen.dart';
+import 'package:task_manager_apps/feature/screen/view/sign%20Up/sign_up.dart';
 import 'package:task_manager_apps/feature/screen/view/splash_screen/splash_screen_view.dart';
 
 class AppRoutes{
@@ -11,6 +18,11 @@ class AppRoutes{
 
     GetPage(name: AppPages.splashScreen, page: ()=>SplashScreenView(), binding: SplashBindings()),
     GetPage(name: AppPages.loginScreen, page: ()=>LoginScreen(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.emailScreen, page: ()=>EmailScreen(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.pinVerification, page: ()=>PinVerificationScreen(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.setPasswordScreen, page: ()=>SetPasswordScreen(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.signUpView, page: ()=>RegisterView(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.newTaskView, page: ()=>NewTaskScreen(), binding: ProfileTaskBinding()),
 
 
   ];
