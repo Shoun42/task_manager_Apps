69a3dca18ca451a9316df883079b184b4ccdc06b
User <shounbabu31@gmail.com>
2026-03-14 13:27:19 +0600

    update

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:task_manager_apps/app/app_pages.dart';
 import 'package:task_manager_apps/feature/screen/bindings/login_screen_binding.dart';
 import 'package:task_manager_apps/feature/screen/bindings/splash_binding.dart';
-import 'package:task_manager_apps/feature/screen/controller/login_screen_controller.dart';
 import 'package:task_manager_apps/feature/screen/view/login_screen/login_screen.dart';
 import 'package:task_manager_apps/feature/screen/view/splash_screen/splash_screen_view.dart';
 
@@ -11,7 +10,7 @@ class AppRoutes{
   static List <GetPage> routes = [
 
     GetPage(name: AppPages.splashScreen, page: ()=>SplashScreenView(), binding: SplashBindings()),
-    GetPage(name: AppPages.loginScreen, page: ()=>LoginScreenView(), binding: LoginScreenBindings()),
+    GetPage(name: AppPages.loginScreen, page: ()=>LoginScreen(), binding: LoginScreenBindings()),
 
 
   ];
